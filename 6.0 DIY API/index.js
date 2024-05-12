72de5baa203dfb9698cb1788cac30756dd32c5f3
unique-mechanic <128269030+unique-mechanic@users.noreply.github.com>
2024-05-12 10:43:02 +0930

    Updated

--- 6.0 DIY API/index.js
+++ 6.0 DIY API/index.js
@@ -32,9 +32,47 @@ app.get("/filter", (req,res)=>{
 
 //4. POST a new joke
 
+app.post("/jokes", (req, res) => {
+  //in postman -> Choose Body ->  x-www-form-urlencoded
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.send(newJoke);
+  
+});
+
 //5. PUT a joke
+app.put("/jokes/:id", (req,res) => {
+  //in postman -> Choose Body ->  x-www-form-urlencoded
+  const id = parseInt(req.params.id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+});
 
 //6. PATCH a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  console.log(jokes[searchIndex]);
+  res.json(replacementJoke);
+});
 
 //7. DELETE Specific joke
 
